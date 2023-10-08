080c3072602ca71a2af9126871aa17fd0065b8fe
Dominic Aguirre <dominicaguirre23@gmail.com>
2023-10-08 15:12:49 -0400

    module 6 complete

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const model = mongoose.model('trips');
+const Trip = mongoose.model('trips');
 
 //
 const tripsList = async (req, res) => {
-    model
+    Trip
         .find({})
         .exec((err, trips) => {
             if(!trips){
@@ -26,7 +26,7 @@ const tripsList = async (req, res) => {
 
 //
 const tripsFindCode = async (req, res) => {
-    model
+    Trip
         .find({ 'code': req.params.tripCode})
         .exec((err, trips) => {
             if(!trips){
@@ -47,7 +47,70 @@ const tripsFindCode = async (req, res) => {
         });
 };
 
+const tripsAddTrip = async (req, res) => {
+    Trip
+        .create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        },
+    (err, trip) => {
+        if (err) {
+            return res
+                .status(404)
+                .json(err);
+        } else {
+            return res
+                .status(201)
+                .json(trip);
+                } 
+    });
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    Trip
+    .findOneAndUpdate({'code': req.params.tripCode }, {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    }, { new: true })
+    .then(trip => {
+        if (!trip) {
+            return res
+                .status(404)
+                .send({
+                    message: "Trip not found with code " + req.params.tripCode
+                });
+        }
+        res.send(trip);
+    }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res
+                .status(404)
+                .send({
+                    message: 'Trip not found with code ' + req.params.tripCode
+                });
+        }
+        return res
+            .status(500)
+            .json(err);
+    });
+}
+
+
 module.exports = {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
